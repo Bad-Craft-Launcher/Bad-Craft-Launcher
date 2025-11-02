da81068b1737ecaeedc71e4d63476484963f5412
TNTyep520 <122156647+TNTyep520@users.noreply.github.com>
2025-11-02 15:59:53 +0800

    Hotfix: 忘记修了（
    tm我一大上午就回退 commit，然后忘记同步更新了结果 git 上去时炸了然后滚去合并分支结果还炸😰

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,6 @@ import "@material/web/dialog/dialog.js";
 import "@material/web/icon/icon.js";
 
 import { createApp } from "vue";
-<<<<<<< HEAD
 import App from "./home.vue";
-=======
-import App from "./App.vue";
-import {
-  provideFluentDesignSystem,
-  fluentButton,
-  fluentCard,
-  fluentSwitch,
-} from "@fluentui/web-components";
-
-provideFluentDesignSystem().register(
-  fluentButton(),
-  fluentCard(),
-  fluentSwitch(),
-);
->>>>>>> 12dd949d5b2bba9556835a1238c5fd7d6fee3f06
 
 createApp(App).mount("#app");
