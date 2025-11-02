3e9fef3de22cd929accb222a57b84779308b3294
TNTyep520 <3345562481@qq.com>
2025-11-02 15:35:01 +0800

    [Revert|Update](Design UI): 滚回到 MD3 设计，但由于网页内不知道怎么使标题栏关闭窗体，只好先把 Tauri 窗体打开了...|更新所有库依赖

---
+++ src/home.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+
+</script>
+
+<template>
+  <h3>Material Card Example</h3>
+  <md-filled-button style="margin-top: 16px;">
+      Material Button
+  </md-filled-button>
+  <md-switch style="margin: 24px auto; display: block;">
+    test
+  </md-switch>
+</template>
+
+<style>
+:root {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+  color: #0f0f0f;
+  background-color: #f6f6f6;
+}
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    color: #f6f6f6;
+    background-color: #2f2f2f;
+  }
+}
+
+</style>
